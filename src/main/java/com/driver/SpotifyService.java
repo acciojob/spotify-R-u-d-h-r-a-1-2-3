e4005aec31903603cc86b4b7f3996286c7cc0e63
L R T J NAIDU <rudhrateja1999@gmail.com>
2023-03-05 17:45:09 +0530

    final

--- src/main/java/com/driver/SpotifyService.java
+++ src/main/java/com/driver/SpotifyService.java
@@ -11,43 +11,43 @@ public class SpotifyService {
 
     SpotifyRepository spotifyRepository = new SpotifyRepository();
 
-    public User CreateUser(String name, String mobile){
+    public User createUser(String name, String mobile){
         return spotifyRepository.createUser(name, mobile);
     }
 
-    public Artist CreateArtist(String name) {
+    public Artist createArtist(String name) {
         return spotifyRepository.createArtist(name);
     }
 
-    public Album CreateAlbum(String title, String artistName) {
+    public Album createAlbum(String title, String artistName) {
         return spotifyRepository.createAlbum(title, artistName);
     }
 
-    public Song CreateSong(String title, String albumName, int length) throws Exception {
+    public Song createSong(String title, String albumName, int length) throws Exception {
         return spotifyRepository.createSong(title, albumName, length);
     }
 
-    public Playlist CreatePlaylistOnLength(String mobile, String title, int length) throws Exception {
+    public Playlist createPlaylistOnLength(String mobile, String title, int length) throws Exception {
         return spotifyRepository.createPlaylistOnLength(mobile, title, length);
     }
 
-    public Playlist CreatePlaylistOnName(String mobile, String title, List<String> songTitles) throws Exception {
+    public Playlist createPlaylistOnName(String mobile, String title, List<String> songTitles) throws Exception {
         return spotifyRepository.createPlaylistOnName(mobile, title, songTitles);
     }
 
-    public Playlist FindPlaylist(String mobile, String playlistTitle) throws Exception {
+    public Playlist findPlaylist(String mobile, String playlistTitle) throws Exception {
         return spotifyRepository.findPlaylist(mobile, playlistTitle);
     }
 
-    public Song LikeSong(String mobile, String songTitle) throws Exception {
+    public Song likeSong(String mobile, String songTitle) throws Exception {
         return spotifyRepository.likeSong(mobile,songTitle);
     }
 
-    public String MostPopularArtist() {
+    public String mostPopularArtist() {
         return spotifyRepository.mostPopularArtist();
     }
 
-    public String MostPopularSong() {
+    public String mostPopularSong() {
         return spotifyRepository.mostPopularSong();
     }
 }
